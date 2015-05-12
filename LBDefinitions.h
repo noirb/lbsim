5fcb72d5229cb76243b0a3df3545a974a616d6f5
noirb <quetchl@gmail.com>
2015-05-12 11:56:17 +0200

    Lattice Velocities & Weights Added according to the tables on pages 3 & 4 of the worksheet

--- LBDefinitions.h
+++ LBDefinitions.h
@@ -1,9 +1,50 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
-  static const int LATTICEVELOCITIES[19][3];
-  static const double LATTICEWEIGHTS[19];
-  static const double C_S;
+  static const int LATTICEVELOCITIES[19][3] {
+                    { 0, -1, -1},
+                    {-1,  0, -1},
+                    { 0,  0, -1},
+                    { 1,  0, -1},
+                    { 0,  1, -1},
+                    {-1, -1,  0},
+                    { 0, -1,  0},
+                    { 1, -1,  0},
+                    {-1,  0,  0},
+                    { 0,  0,  0},
+                    { 1,  0,  0},
+                    {-1,  1,  0},
+                    { 0,  1,  0},
+                    { 1,  1,  0},
+                    { 0, -1,  1},
+                    {-1,  0,  1},
+                    { 0,  0,  1},
+                    { 1,  0,  1},
+                    { 0,  1,  1}
+  };
+
+  static const double LATTICEWEIGHTS[19] {
+                     1.0 / 36.0,
+                     1.0 / 36.0,
+                     2.0 / 36.0,
+                     1.0 / 36.0,
+                     1.0 / 36.0,
+                     1.0 / 36.0,
+                     2.0 / 36.0,
+                     1.0 / 36.0,
+                     2.0 / 36.0,
+                    12.0 / 36.0,
+                     2.0 / 36.0,
+                     1.0 / 36.0,
+                     2.0 / 36.0,
+                     1.0 / 36.0,
+                     1.0 / 36.0,
+                     1.0 / 36.0,
+                     2.0 / 36.0,
+                     1.0 / 36.0,
+                     1.0 / 36.0
+  };
+  static const double C_S = 1 / sqrt(3.0);
 
 #endif
 
