2fba237f6d1b2587b863e3ae1f538b1ed268cfce
noirb <quetchl@gmail.com>
2015-05-12 12:30:58 +0200

    Add INDEXOF macro INDEXOF(xlength, x, y, z, i) will return the correct array index for the given point (this only works because our grid is always xlength X xlength X xlength)
    Also fixed a couple typos in LBDefinitions.h

--- LBDefinitions.h
+++ LBDefinitions.h
@@ -1,7 +1,7 @@
 #ifndef _LBDEFINITIONS_H_
 #define _LBDEFINITIONS_H_
 
-  static const int LATTICEVELOCITIES[19][3] {
+  static const int LATTICEVELOCITIES[19][3] = {
                     { 0, -1, -1},
                     {-1,  0, -1},
                     { 0,  0, -1},
@@ -23,7 +23,7 @@
                     { 0,  1,  1}
   };
 
-  static const double LATTICEWEIGHTS[19] {
+  static const double LATTICEWEIGHTS[19] = {
                      1.0 / 36.0,
                      1.0 / 36.0,
                      2.0 / 36.0,
@@ -44,7 +44,12 @@
                      1.0 / 36.0,
                      1.0 / 36.0
   };
-  static const double C_S = 1 / sqrt(3.0);
+  static const double C_S = 1.0 / 1.73205080757; // 1 / sqrt(3.0);
+
+  typedef enum { FLUID, NO_SLIP, MOVING_WALL } cell_flag;
+
+  // simplify indexing into our arrays
+  #define INDEXOF(xlength, x, y, z, i) 19 * (z * xlength * xlength + y * xlength + x) + i
 
 #endif
 
