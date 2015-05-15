537ee68fee6d0d81e1b3d4c564d5e76978521f9b
noirb <quetchl@gmail.com>
2015-05-15 18:12:10 +0200

    Missed a line in the last commit...

--- initLB.c
+++ initLB.c
@@ -20,7 +20,6 @@ int readParameters(int *xlength, double *tau, double *velocityWall, int *timeste
 
 void initialiseFields(double *collideField, double *streamField, int *flagField, int xlength){
 
-  flagField = (int*) malloc(sizeof(int) * pow(xlength+2, 3));
         /* Initialize Arrays */
   for (int i = 0; i < xlength; i++)
   {
@@ -34,7 +33,7 @@ void initialiseFields(double *collideField, double *streamField, int *flagField,
             collideField[INDEXOF(xlength, i, j, k, l)] = LATTICEWEIGHTS[l];
             streamField[INDEXOF(xlength, i, j, k, l)] = LATTICEWEIGHTS[l];
         }
-        
+
         // set flags at (i,j,k)
         flagField[FINDEXOF(xlength, i, j, k)] = k == 0 ? NO_SLIP :                      // bottom layer
                                                 k == xlength - 1 ? MOVING_WALL :        // top layer
