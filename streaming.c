35368a0451f7064a836f964c0cbd4a5e0267d693
noirb <quetchl@gmail.com>
2015-05-15 19:02:44 +0200

    Fix merge error in boundary.c`

--- streaming.c
+++ streaming.c
@@ -2,11 +2,13 @@
 #include "LBDefinitions.h"
 
 void doStreaming(double *collideField, double *streamField,int *flagField,int xlength){
+int flag;
     for (int i = 1; i <= xlength; i++){
         for (int j = 1; j <= xlength; j++){
             for (int k = 1; k <= xlength; k++){
                 for (int dir = 1; dir <= NUMBER_OF_LATTICE_DIRECTIONS; dir++){
-                    streamField[INDEXOF(xlength, i, j, k, dir)] = collideField[INDEXOF(xlength, i+LATTICEVELOCITIES[dir][0], j+LATTICEVELOCITIES[dir][1], k+LATTICEVELOCITIES[dir][2], dir)];
+                    if ((flag = flagField[FINDEXOF(xlength, i, j, k)]) == FLUID)
+                        streamField[INDEXOF(xlength, i, j, k, dir)] = collideField[INDEXOF(xlength, i+LATTICEVELOCITIES[dir][0], j+LATTICEVELOCITIES[dir][1], k+LATTICEVELOCITIES[dir][2], dir)];
                 }
             }
         }
