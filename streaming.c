755567b2a4e67d53edb51937916a73c52d6a0ed0
tamas <quetchl@gmail.com>
2015-05-16 21:36:10 +0200

    Rearrange doStreaming The check to see if we're in a fluid cell was being performed Q times per cell. This change lets us just check once per cell.

--- streaming.c
+++ streaming.c
@@ -1,14 +1,16 @@
 #include "streaming.h"
 #include "LBDefinitions.h"
 
-void doStreaming(double *collideField, double *streamField,int *flagField,int xlength)
+void doStreaming(double *collideField, double *streamField, int *flagField, int xlength)
 {
     for (int i = 1; i <= xlength; i++){
         for (int j = 1; j <= xlength; j++){
             for (int k = 1; k <= xlength; k++){
-                for (int dir = 0; dir < NUMBER_OF_LATTICE_DIRECTIONS; dir++){
-                    if (flagField[FINDEXOF(xlength, i, j, k)] == FLUID)
-                        streamField[INDEXOF(xlength, i, j, k, dir)] = collideField[INDEXOF(xlength, i+LATTICEVELOCITIES[dir][0], j+LATTICEVELOCITIES[dir][1], k+LATTICEVELOCITIES[dir][2], dir)];
+                if (flagField[FINDEXOF(xlength, i, j, k)] == FLUID)
+                {
+                    for (int dir = 0; dir < NUMBER_OF_LATTICE_DIRECTIONS; dir++){
+                        streamField[INDEXOF(xlength, i, j, k, dir)] = collideField[INDEXOF(xlength, i-LATTICEVELOCITIES[dir][0], j-LATTICEVELOCITIES[dir][1], k-LATTICEVELOCITIES[dir][2], dir)];
+                    }
                 }
             }
         }
