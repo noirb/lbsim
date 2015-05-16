34f0c440e2d2f471f97a0f5310e2d1dfcddf3e6c
tamas <quetchl@gmail.com>
2015-05-16 19:26:03 +0200

    Remove unused variable 'flag' from doStreaming This was only used to check a condition, which can be done directly without creating a new variable.

--- streaming.c
+++ streaming.c
@@ -1,13 +1,13 @@
 #include "streaming.h"
 #include "LBDefinitions.h"
 
-void doStreaming(double *collideField, double *streamField,int *flagField,int xlength){
-int flag;
+void doStreaming(double *collideField, double *streamField,int *flagField,int xlength)
+{
     for (int i = 1; i <= xlength; i++){
         for (int j = 1; j <= xlength; j++){
             for (int k = 1; k <= xlength; k++){
                 for (int dir = 0; dir < NUMBER_OF_LATTICE_DIRECTIONS; dir++){
-                    if ((flag = flagField[FINDEXOF(xlength, i, j, k)]) == FLUID)
+                    if (flagField[FINDEXOF(xlength, i, j, k)] == FLUID)
                         streamField[INDEXOF(xlength, i, j, k, dir)] = collideField[INDEXOF(xlength, i+LATTICEVELOCITIES[dir][0], j+LATTICEVELOCITIES[dir][1], k+LATTICEVELOCITIES[dir][2], dir)];
                 }
             }
